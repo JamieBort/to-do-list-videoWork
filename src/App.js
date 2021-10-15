4c0a35a8707896880bec4f25cd3b28f03a84b55c
Jamie Bort <jamiebort@gmail.com>
2021-10-14 22:33:07 -0400

    Modify the App.js file.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
 
+const todoList = [
+  {
+    id: 1,
+    title: "Complete assignment"
+  },
+  {
+    id: 2,
+    title: "Random title"
+  },
+  {
+    id: 3,
+    title: "Parts list"},
+]
+
 function App() {
+  const todoListItems = todoList.map((item) =>
+  <li key={item.id}>{item.title}</li>
+);
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To-Do List</h1>
+      <ul>
+      {todoListItems}
+      {/* {listItems} */}
+      </ul>
     </div>
-  );
+
+    );
 }
 
 export default App;
