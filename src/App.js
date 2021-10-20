34849b18e5b3ffce8e483b6adff79bd3e451b8b0
Jamie Bort <jamiebort@gmail.com>
2021-10-20 10:54:35 -0400

    Create 'Add Todo Form' Component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 function App() {
   return (
-    <TodoList />
+    <div>
+      <h1>To-Do List</h1>
+      <AddTodoForm />
+      <TodoList />
+    </div>
   );
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,17 +1,3 @@
-// Inside /src directory, create a new file called TodoList.js
-//  Open /src/TodoList.js
-//  Create a new functional React component (see below)
-//  Import React from "react" npm package
-//  Declare a function named TodoList
-//  Export TodoList function as default module
-//  Add a multi-line return statement to your TodoList function (this is where we will insert JSX)
-// hint: use parenthesis for multi-line return
-//  Move list JSX from App.js to TodoList.js (see below)
-//  Open /src/App.js
-//  Cut (copy and remove) the entire list element (<ul>) code
-//  Open /src/TodoList.js
-//  Inside the multi-line return, paste the list element (<ul>) code
-
 import React from 'react';
 
 const todoList = [
@@ -51,7 +37,6 @@ const todoListItems = todoList.map((item) =>
 function TodoList() {
     return (
         <div>
-            <h1>To-Do List</h1>
             <ul>
                 {todoListItems}
             </ul>
